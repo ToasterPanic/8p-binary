b74f52581c23190ec03d9d36beff756e7ce31b32
ToasterPanic <me@owouw.us>
2025-02-14 20:00:50 -0500

    FINALLY! SHIT

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-./fteqw-sv64 -dedicated 2 -game nzp +exec sv.cfg +map town
+chmod a+x ./fteqw-sv64
+
+./fteqw-sv64 -dedicated 2 -game nzp +exec sv.cfg +map town +sv_enablechatplugins 1 +sv_maprotationmode 1
